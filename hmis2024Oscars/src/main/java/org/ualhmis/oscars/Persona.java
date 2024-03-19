3e3df26468da611dd57fc9aeb935d950e9661d5a
jjcanada <jjcanada@ual.es>
2024-03-19 13:26:52 +0100

    equals en clase Persona

--- hmis2024Oscars/src/main/java/org/ualhmis/oscars/Persona.java
+++ hmis2024Oscars/src/main/java/org/ualhmis/oscars/Persona.java
@@ -28,5 +28,46 @@ class Persona {
     public String getFechaNacimiento() {
         return fechaNacimiento;
     }
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((fechaNacimiento == null) ? 0 : fechaNacimiento.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime * result + ((sexo == null) ? 0 : sexo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Persona other = (Persona) obj;
+		if (fechaNacimiento == null) {
+			if (other.fechaNacimiento != null)
+				return false;
+		} else if (!fechaNacimiento.equals(other.fechaNacimiento))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (sexo == null) {
+			if (other.sexo != null)
+				return false;
+		} else if (!sexo.equals(other.sexo))
+			return false;
+		return true;
+	}
+    
+    
+    
+    
 }
 
