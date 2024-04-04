5c879ad7bd44a552b2fb4c90cb28a0bef3acd0f1
crv520-1 <crv520@inlumine.ual.es>
2024-04-04 20:14:13 +0200

    testPremio correcto

--- hmis2024Oscars/src/test/java/org/ualhmis/oscars/TestPremio.java
+++ hmis2024Oscars/src/test/java/org/ualhmis/oscars/TestPremio.java
@@ -20,6 +20,9 @@ public class TestPremio {
 		Premio p2 = new Premio("Pelicula del anio");
 		p2.addNominada(new Pelicula("Guardianes de la galaxia", null));
 	    p2.addNominada(new Pelicula("Barbie", null));
-		assertEquals(p1.getNominadas(),p2.getNominadas());
+		assertEquals(p1.getNominadas().get(0).getTitulo(), "Avatar");
+		assertEquals(p1.getNominadas().get(1).getTitulo(), "Oppenheimer");
+		assertEquals(p2.getNominadas().get(0).getTitulo(), "Guardianes de la galaxia");
+		assertEquals(p2.getNominadas().get(1).getTitulo(), "Barbie");
 	}
 }
